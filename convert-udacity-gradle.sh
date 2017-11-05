f36f1d0685640a4f9c05e4b5190d9b3b96681c6c
Denis (shaoran-win8) <18046412+gshadows@users.noreply.github.com>
2017-11-05 22:13:23 +0300

    Add project Sunshine support. [+] Gradle wrapper configuration update. [+] Additional libraries support. [.] Improve regex.

--- convert-udacity-gradle.sh
+++ convert-udacity-gradle.sh
@@ -2,30 +2,50 @@
 
 # Some templates.
 num="[0-9]\+"
-dots3="$num\.$num\.$num"
-optrc="\(-rc[0-9]\+\)\?"
+optdot="\(\.$num\)\?"
+optrc="\(\-rc$num\)\?"
+optbeta="\(\-beta$num\)\?"
+vers="$num\(\.$num\)*$optrc$optbeta"
+
+mainpath="*/*"
+apppath="$mainpath/app"
 
 
 # ==== MAIN GRADLE FILE ====
 
 # Update gradle plugin version fro Android Studio 3.
 prefix="com\.android\.tools\.build\:gradle\:"
-sub="$dots3$optrc"
+sub="$vers"
 goal="3.0.0-rc2"
-find */*/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+find $mainpath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+
+# Update gradle wrapper.
+sub="gradle\-$vers\-"
+goal="gradle-4.1-"
+find $mainpath/gradle/wrapper/gradle-wrapper.properties -exec sed -i "s/$sub/$goal/g" {} \;
 
 # Remove build directory change.
 #sub="\n.*String osName =.*\n.*\n.*\n.*}.*\n"
 #goal="\n"
-#find */*/build.gradle -exec perl -0777 -pi -e "s/$sub/$goal/g" {} \;
+#find $mainpath/build.gradle -exec perl -0777 -pi -e "s/$sub/$goal/g" {} \;
+
+# Change build directory location.
+sub="C\:\/tmp\/"
+goal="E:\/DENIS\/Projects\/Android\/GoogleChallenge\/_build\/"
+find $mainpath/build.gradle -exec sed -i "s/$sub/$goal/g" {} \;
 
 # Add Google repository.
 sub="jcenter\(\)"
 goal="google()\n        jcenter()"
-find */*/build.gradle -exec perl -0777 -pi -e "s/$sub/$goal/g" {} \;
+find $mainpath/build.gradle -exec perl -0777 -pi -e "s/$sub/$goal/g" {} \;
+
+# Remove duplicate Google repository.
+sub="google\(\)\n        google\(\)"
+goal="google()"
+find $mainpath/build.gradle -exec perl -0777 -pi -e "s/$sub/$goal/g" {} \;
 
 # Remove backup file.
-find */*/build.gradle.bak -exec rm -f {} \;
+find $mainpath/build.gradle.bak -exec rm -f {} \;
 
 # ==== APPLICATION GRADLE FILE ====
 
@@ -33,28 +53,55 @@ find */*/build.gradle.bak -exec rm -f {} \;
 prefix="compileSdkVersion "
 sub="\s*$num"
 goal="26"
-find */*/app/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
 
 # Set build tools version.
 prefix="buildToolsVersion "
-sub="\s*[\"\']$dots3[\"\']"
+sub="\s*[\"\']$vers[\"\']"
 goal="\"27.0.0\""
-find */*/app/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
 
 # Set minimal SDK version.
 prefix="minSdkVersion "
 sub="\s*$num"
 goal="15"
-find */*/app/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
 
 # Set target SDK version.
 prefix="targetSdkVersion "
 sub="\s*$num"
 goal="26"
-find */*/app/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
 
-# Update gradle plugin version fro Android Studio 3.
+# Update appcompat library version.
 prefix="com\.android\.support\:appcompat\-v7\:"
-sub="$dots3$optrc"
+sub="$vers"
 goal="26.1.0"
-find */*/app/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+
+# Update support-annotations library version.
+prefix="com\.android\.support\:support\-annotations\:"
+sub="$vers"
+goal="26.1.0"
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+
+# Update test:runner library version.
+prefix="com\.android\.support\.test\:runner\:"
+sub="$vers"
+goal="1.0.1"
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+
+# Update test:rules library version.
+prefix="com\.android\.support\.test\:rules\:"
+sub="$vers"
+goal="1.0.1"
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+
+# Update constraints layout library version.
+prefix="com\.android\.support\.constraint\:constraint\-layout\:"
+sub="$vers"
+goal="1.1.0-beta3"
+find $apppath/build.gradle -exec sed -i "s/$prefix$sub/$prefix$goal/g" {} \;
+
+# Remove backup file.
+find $apppath/build.gradle.bak -exec rm -f {} \;
